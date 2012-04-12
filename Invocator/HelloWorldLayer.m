46ab68300e1bf7e04066a9e6f192580eeae6a418
Toru Hisai <toru@torus.jp>
2012-04-13 00:08:14 +0900

    introduced NSInvocation

--- Invocator/HelloWorldLayer.m
+++ Invocator/HelloWorldLayer.m
@@ -47,6 +47,32 @@
 		// add the label as a child to this Layer
 		[self addChild: label];
 	}
+    
+    NSString *str = @"Hoge Fuga";
+    SEL sel = sel_getUid("characterAtIndex:");
+    NSMethodSignature *sig = [str methodSignatureForSelector:sel];
+    NSInvocation *inv = [NSInvocation invocationWithMethodSignature:sig];
+    NSUInteger numarg = [sig numberOfArguments];
+    NSLog(@"Number of arguments = %d", numarg);
+    
+    for (int i = 0; i < numarg; i++) {
+        const char *t = [sig getArgumentTypeAtIndex:i];
+        NSLog(@"arg %d: %s", i, t);
+    }
+
+    [inv setTarget:str];
+    [inv setSelector:sel];
+    NSUInteger arg1 = 5;
+    [inv setArgument:&arg1 atIndex:2];
+    [inv invoke];
+    
+    NSUInteger len = [[inv methodSignature] methodReturnLength];
+    const char *rettype = [sig methodReturnType];
+    NSLog(@"ret type = %s", rettype);
+    void *buffer = malloc(len);
+    [inv getReturnValue:buffer];
+    NSLog(@"ret = %c", *(unichar*)buffer);
+    
 	return self;
 }
 
